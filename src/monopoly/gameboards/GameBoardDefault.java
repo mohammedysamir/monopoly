594954a57ab465f692f328d20898339fe3a26e2c
Mohammed Yasser <mohammedre4a@gmail.com>
2022-05-30 18:21:05 +0200

    refactor: refactor GameBoardDefault constructor, remove duplicate code and encapsulate it in functions + added method docs

--- src/monopoly/gameboards/GameBoardDefault.java
+++ src/monopoly/gameboards/GameBoardDefault.java
@@ -15,258 +15,106 @@ import monopoly.enums.CardType;
 import monopoly.enums.ColorGroup;
 
 public class GameBoardDefault extends GameBoard {
+
+
+    int rrBaseRent = 25;
+    int rrPrice = 200;
+
     public GameBoardDefault() {
         super();
-        
-        int rrBaseRent = 25;
-        int rrPrice = 200;
-        
-        PropertyCell dp1 = new PropertyCell();
-        CardCell cc1 = new CardCell(CardType.CC, "Community Chest 1");
-        PropertyCell dp2 = new PropertyCell();
-        PropertyCell dp3 = new PropertyCell();
-        RailRoadCell rr1 = new RailRoadCell();
-        PropertyCell lb1 = new PropertyCell();
-        CardCell c1 = new CardCell(CardType.CHANCE, "Chance 1");
-        PropertyCell lb2 = new PropertyCell();
-        PropertyCell lb3 = new PropertyCell();
+
+        //Initialize property cells
+        PropertyCell mediterraneanAvenueCell = initiatePropertyCell(60, ColorGroup.PURPLE, 50, "Mediterranean Avenue", 2);
+        PropertyCell balticAvenueCell = initiatePropertyCell(60, ColorGroup.PURPLE, 50, "Baltic Avenue", 4);
+        PropertyCell sarahAvenueCell = initiatePropertyCell(60, ColorGroup.PURPLE, 50, "Sarah Avenue", 4);
+        PropertyCell orientalAvenueCell = initiatePropertyCell(100, ColorGroup.TEAL, 50, "Oriental Avenue", 6);
+        PropertyCell vermontAvenueCell = initiatePropertyCell(100, ColorGroup.TEAL, 50, "Vermont Avenue", 6);
+        PropertyCell connecticutAvenueCell = initiatePropertyCell(120, ColorGroup.TEAL, 50, "Connecticut Avenue", 8);
+        PropertyCell charlesPlaceCell = initiatePropertyCell(140, ColorGroup.FUCHSIA, 100, "St. Charles Place", 10);
+        PropertyCell statesAvenueCell = initiatePropertyCell(140, ColorGroup.FUCHSIA, 100, "States Avenue", 10);
+        PropertyCell virginiaAvenueCell = initiatePropertyCell(160, ColorGroup.FUCHSIA, 100, "Virginia Avenue", 12);
+        PropertyCell jamesAvenueCell = initiatePropertyCell(180, ColorGroup.MAROON, 100, "St. James Avenue", 14);
+        PropertyCell tennesseeAvenueCell = initiatePropertyCell(180, ColorGroup.MAROON, 100, "Tennessee Avenue", 14);
+        PropertyCell newYorkAvenueCell = initiatePropertyCell(200, ColorGroup.MAROON, 100, "New York Avenue", 16);
+        PropertyCell kentuckyAvenueCell = initiatePropertyCell(220, ColorGroup.RED, 150, "Kentucky Avenue", 18);
+        PropertyCell indianaAvenueCell = initiatePropertyCell(220, ColorGroup.RED, 150, "Indiana Avenue", 18);
+        PropertyCell illinoisAvenueCell = initiatePropertyCell(240, ColorGroup.RED, 150, "Illinois Avenue", 20);
+        PropertyCell atlanticAvenueCell = initiatePropertyCell(260, ColorGroup.ORANGE, 150, "Atlantic Avenue", 22);
+        PropertyCell ventnorAvenueCell = initiatePropertyCell(260, ColorGroup.ORANGE, 150, "Ventnor Avenue", 22);
+        PropertyCell marvinGardensCell = initiatePropertyCell(280, ColorGroup.ORANGE, 150, "Marvin Gardens", 24);
+        PropertyCell pacificAvenueCell = initiatePropertyCell(300, ColorGroup.GREEN, 200, "Pacific Avenue", 26);
+        PropertyCell northCarolinaAvenueCell = initiatePropertyCell(300, ColorGroup.GREEN, 200, "North Carolina Avenue", 26);
+        PropertyCell pennsylvaniaAvenueCell = initiatePropertyCell(320, ColorGroup.GREEN, 200, "Pennsylvania Avenue", 28);
+        PropertyCell parkPlaceCell = initiatePropertyCell(350, ColorGroup.BLUE, 200, "Park Place", 35);
+        PropertyCell drightPlaceCell = initiatePropertyCell(350, ColorGroup.BLUE, 200, "Dright Place", 35);
+        PropertyCell boardWalkCell = initiatePropertyCell(400, ColorGroup.BLUE, 200, "Boardwalk", 50);
+
+        //initiate card cells
+        CardCell communityChestCell1 = initiateCardCell(CardType.CC, "Community Chest 1");
+        CardCell chanceCell1 = initiateCardCell(CardType.CHANCE, "Chance 1");
+        CardCell communityChestCell2 = initiateCardCell(CardType.CC, "Community Chest 2");
+        CardCell chanceCell2 = initiateCardCell(CardType.CHANCE, "Chance 2");
+        CardCell communityChestCell3 = initiateCardCell(CardType.CC, "Community Chest 3");
+        CardCell chanceCell3 = initiateCardCell(CardType.CHANCE, "Chance 3");
+
+        //initiate railroad cells
+        RailRoadCell readingRailRoadCell = initiateRailRoadCell("Reading RailRoad");
+        RailRoadCell pennsylvaniaRailRoadCell = initiateRailRoadCell("Pennsylvania RailRoad");
+        RailRoadCell b_oRailRoadCell = initiateRailRoadCell("B. & O. RailRoad");
+        RailRoadCell shortLineRailRoadCell = initiateRailRoadCell("Short Line");
+
+        //define utility cells
         JailCell jail = new JailCell();
-        PropertyCell p1 = new PropertyCell();
-        UtilityCell u1 = new UtilityCell();
-        PropertyCell p2 = new PropertyCell();
-        PropertyCell p3 = new PropertyCell();
-        RailRoadCell rr2 = new RailRoadCell();
-        PropertyCell o1 = new PropertyCell();
-        CardCell cc2 = new CardCell(CardType.CC, "Community Chest 2");
-        PropertyCell o2 = new PropertyCell();
-        PropertyCell o3 = new PropertyCell();
-        FreeParkingCell fp = new FreeParkingCell();
-        PropertyCell r1 = new PropertyCell();
-        CardCell c2 = new CardCell(CardType.CHANCE, "Chance 2");
-        PropertyCell r2 = new PropertyCell();
-        PropertyCell r3 = new PropertyCell();
-        RailRoadCell rr3 = new RailRoadCell();
-        PropertyCell y1 = new PropertyCell();
-        PropertyCell y2 = new PropertyCell();
-        UtilityCell u2 = new UtilityCell();
-        PropertyCell y3 = new PropertyCell();
+        FreeParkingCell freeParkingCell = new FreeParkingCell();
         GoToJailCell goToJail = new GoToJailCell();
-        PropertyCell g1 = new PropertyCell();
-        PropertyCell g2 = new PropertyCell();
-        CardCell cc3 = new CardCell(CardType.CC, "Community Chest 3");
-        PropertyCell g3 = new PropertyCell();
-        RailRoadCell rr4 = new RailRoadCell();
-        CardCell c3 = new CardCell(CardType.CHANCE, "Chance 3");
-        
-        PropertyCell db1 = new PropertyCell();
-        PropertyCell db2 = new PropertyCell();
-        PropertyCell db3 = new PropertyCell();
-        
-        dp1.setPrice(60);
-        dp1.setColorGroup(ColorGroup.PURPLE);
-        dp1.setHousePrice(50);
-        dp1.setName("Mediterranean Avenue");
-        dp1.setRent(2);
-        
-        dp2.setPrice(60);
-        dp2.setColorGroup(ColorGroup.PURPLE);
-        dp2.setHousePrice(50);
-        dp2.setName("Baltic Avenue");
-        dp2.setRent(4);
-        
-        dp3.setPrice(60);
-        dp3.setColorGroup(ColorGroup.PURPLE);
-        dp3.setHousePrice(50);
-        dp3.setName("Sarah Avenue");
-        dp3.setRent(4);
-        
-        lb1.setPrice(100);
-        lb1.setColorGroup(ColorGroup.TEAL);
-        lb1.setHousePrice(50);
-        lb1.setName("Oriental Avenue");
-        lb1.setRent(6);
-
-        lb2.setPrice(100);
-        lb2.setColorGroup(ColorGroup.TEAL);
-        lb2.setHousePrice(50);
-        lb2.setName("Vermont Avenue");
-        lb2.setRent(6);
-
-        lb3.setPrice(120);
-        lb3.setColorGroup(ColorGroup.TEAL);
-        lb3.setHousePrice(50);
-        lb3.setName("Connecticut Avenue");
-        lb3.setRent(8);
-        
-        p1.setPrice(140);
-        p1.setColorGroup(ColorGroup.FUCHSIA);
-        p1.setHousePrice(100);
-        p1.setName("St. Charles Place");
-        p1.setRent(10);
-        
-        p2.setPrice(140);
-        p2.setColorGroup(ColorGroup.FUCHSIA);
-        p2.setHousePrice(100);
-        p2.setName("States Avenue");
-        p2.setRent(10);
-        
-        p3.setPrice(160);
-        p3.setColorGroup(ColorGroup.FUCHSIA);
-        p3.setHousePrice(100);
-        p3.setName("Virginia Avenue");
-        p3.setRent(12);
-        
-        o1.setPrice(180);
-        o1.setColorGroup(ColorGroup.MAROON);
-        o1.setHousePrice(100);
-        o1.setName("St. James Avenue");
-        o1.setRent(14);
-        
-        o2.setPrice(180);
-        o2.setColorGroup(ColorGroup.MAROON);
-        o2.setHousePrice(100);
-        o2.setName("Tennessee Avenue");
-        o2.setRent(14);
-        
-        o3.setPrice(200);
-        o3.setColorGroup(ColorGroup.MAROON);
-        o3.setHousePrice(100);
-        o3.setName("New York Avenue");
-        o3.setRent(16);
-        
-        r1.setPrice(220);
-        r1.setColorGroup(ColorGroup.RED);
-        r1.setHousePrice(150);
-        r1.setName("Kentucky Avenue");
-        r1.setRent(18);
-        
-        r2.setPrice(220);
-        r2.setColorGroup(ColorGroup.RED);
-        r2.setHousePrice(150);
-        r2.setName("Indiana Avenue");
-        r2.setRent(18);
-        
-        r3.setPrice(240);
-        r3.setColorGroup(ColorGroup.RED);
-        r3.setHousePrice(150);
-        r3.setName("Illinois Avenue");
-        r3.setRent(20);
-        
-        y1.setPrice(260);
-        y1.setColorGroup(ColorGroup.ORANGE);
-        y1.setHousePrice(150);
-        y1.setName("Atlantic Avenue");
-        y1.setRent(22);
-        
-        y2.setPrice(260);
-        y2.setColorGroup(ColorGroup.ORANGE);
-        y2.setHousePrice(150);
-        y2.setName("Ventnor Avenue");
-        y2.setRent(22);
-        
-        y3.setPrice(280);
-        y3.setColorGroup(ColorGroup.ORANGE);
-        y3.setHousePrice(150);
-        y3.setName("Marvin Gardens");
-        y3.setRent(24);
-        
-        g1.setPrice(300);
-        g1.setColorGroup(ColorGroup.GREEN);
-        g1.setHousePrice(200);
-        g1.setName("Pacific Avenue");
-        g1.setRent(26);
-        
-        g2.setPrice(300);
-        g2.setColorGroup(ColorGroup.GREEN);
-        g2.setHousePrice(200);
-        g2.setName("North Carolina Avenue");
-        g2.setRent(26);
-        
-        g3.setPrice(320);
-        g3.setColorGroup(ColorGroup.GREEN);
-        g3.setHousePrice(200);
-        g3.setName("Pennsylvania Avenue");
-        g3.setRent(28);
-        
-        db1.setPrice(350);
-        db1.setColorGroup(ColorGroup.BLUE);
-        db1.setHousePrice(200);
-        db1.setName("Park Place");
-        db1.setRent(35);
-        
-        db2.setPrice(350);
-        db2.setColorGroup(ColorGroup.BLUE);
-        db2.setHousePrice(200);
-        db2.setName("Dright Place");
-        db2.setRent(35);
-        
-        db3.setPrice(400);
-        db3.setColorGroup(ColorGroup.BLUE);
-        db3.setHousePrice(200);
-        db3.setName("Boardwalk");
-        db3.setRent(50);
-        
-        rr1.setName("Reading Railroad");
-        rr1.setBaseRent(rrBaseRent);
-        rr1.setPrice(rrPrice);
- 
-        rr2.setName("Pennsylvania Railroad");
-        rr2.setBaseRent(rrBaseRent);
-        rr2.setPrice(rrPrice);
-        
-        rr3.setName("B. & O. RailRoad");
-        rr3.setBaseRent(rrBaseRent);
-        rr3.setPrice(rrPrice);
-        
-        rr4.setName("Short Line");
-        rr4.setBaseRent(rrBaseRent);
-        rr4.setPrice(rrPrice);
-        
+        UtilityCell electricCompanyUtilityCell = new UtilityCell();
+        UtilityCell waterWorksUtilityCell = new UtilityCell();
         UtilityCell.setPrice(150);
-        
-        u1.setName("Electric Company");
-        u2.setName("Water Works");
-        
-        super.addCell(dp1);
-        super.addCell(cc1);
-        super.addCell(dp2);
-        super.addCell(dp3);
-        super.addCell(rr1);
-        super.addCell(lb1);
-        super.addCell(c1);
-        super.addCell(lb2);
-        super.addCell(lb3);
+
+        electricCompanyUtilityCell.setName("Electric Company");
+        waterWorksUtilityCell.setName("Water Works");
+
+
+        super.addCell(mediterraneanAvenueCell);
+        super.addCell(communityChestCell1);
+        super.addCell(balticAvenueCell);
+        super.addCell(sarahAvenueCell);
+        super.addCell(readingRailRoadCell);
+        super.addCell(orientalAvenueCell);
+        super.addCell(chanceCell1);
+        super.addCell(vermontAvenueCell);
+        super.addCell(connecticutAvenueCell);
         super.addCell(jail);
-        super.addCell(p1);
-        super.addCell(u1);
-        super.addCell(p2);
-        super.addCell(p3);
-        super.addCell(rr2);
-        super.addCell(o1);
-        super.addCell(cc2);
-        super.addCell(o2);
-        super.addCell(o3);
-        super.addCell(fp);
-        super.addCell(r1);
-        super.addCell(c2);
-        super.addCell(r2);
-        super.addCell(r3);
-        super.addCell(rr3);
-        super.addCell(y1);
-        super.addCell(y2);
-        super.addCell(u2);
-        super.addCell(y3);
+        super.addCell(charlesPlaceCell);
+        super.addCell(electricCompanyUtilityCell);
+        super.addCell(statesAvenueCell);
+        super.addCell(virginiaAvenueCell);
+        super.addCell(pennsylvaniaRailRoadCell);
+        super.addCell(jamesAvenueCell);
+        super.addCell(communityChestCell2);
+        super.addCell(tennesseeAvenueCell);
+        super.addCell(newYorkAvenueCell);
+        super.addCell(freeParkingCell);
+        super.addCell(kentuckyAvenueCell);
+        super.addCell(chanceCell2);
+        super.addCell(indianaAvenueCell);
+        super.addCell(illinoisAvenueCell);
+        super.addCell(b_oRailRoadCell);
+        super.addCell(atlanticAvenueCell);
+        super.addCell(ventnorAvenueCell);
+        super.addCell(waterWorksUtilityCell);
+        super.addCell(marvinGardensCell);
         super.addCell(goToJail);
-        super.addCell(g1);
-        super.addCell(g2);
-        super.addCell(cc3);
-        super.addCell(g3);
-        super.addCell(rr4);
-        super.addCell(c3);
-        super.addCell(db1);
-        super.addCell(db2);
-        super.addCell(db3);
-        
+        super.addCell(pacificAvenueCell);
+        super.addCell(northCarolinaAvenueCell);
+        super.addCell(communityChestCell3);
+        super.addCell(pennsylvaniaAvenueCell);
+        super.addCell(shortLineRailRoadCell);
+        super.addCell(chanceCell3);
+        super.addCell(parkPlaceCell);
+        super.addCell(drightPlaceCell);
+        super.addCell(boardWalkCell);
+
         super.addCard(new MoneyCard("Win $50", 50, CardType.CC));
         super.addCard(new MoneyCard("Win $20", 20, CardType.CC));
         super.addCard(new MoneyCard("Win $10", 10, CardType.CC));
@@ -283,7 +131,52 @@ public class GameBoardDefault extends GameBoard {
         super.addCard(new MoneyCard("Lose $50", -50, CardType.CHANCE));
         super.addCard(new JailCard(CardType.CHANCE));
         super.addCard(new MovePlayerCard("Illinois Avenue", CardType.CHANCE));
-        
+
         super.shuffleCards();
+    }
+
+    /**
+     * Function used to initialize PropertyCell objects with params
+     *
+     * @param price      : determine cell's price
+     * @param color      : determine cell's color
+     * @param housePrice : determine cell's house build price
+     * @param Name       : determine cell's name
+     * @param rentPrice  : determine cell's rent price
+     * @return PropertyCell object associated with sent parameters
+     */
+    public PropertyCell initiatePropertyCell(int price, ColorGroup color, int housePrice, String Name, int rentPrice) {
+        PropertyCell propertyCell = new PropertyCell();
+        propertyCell.setPrice(price);
+        propertyCell.setColorGroup(color);
+        propertyCell.setHousePrice(housePrice);
+        propertyCell.setName(Name);
+        propertyCell.setRent(rentPrice);
+        return propertyCell;
+    }
+
+    /**
+     * Function used to initialize CardCell with params
+     *
+     * @param cardType : determine card type {CC or Chance}
+     * @param cardName : determine card's Name
+     * @return CardCell object with specified name and type
+     */
+    public CardCell initiateCardCell(CardType cardType, String cardName) {
+        return new CardCell(cardType, cardName);
+    }
+
+    /**
+     * Function used to initialize RailRoad cells with Name
+     *
+     * @param railRoadName: Determine cell's name
+     * @return RailRoadCell object with specified name
+     */
+    public RailRoadCell initiateRailRoadCell(String railRoadName) {
+        RailRoadCell railRoadCell = new RailRoadCell();
+        railRoadCell.setName(railRoadName);
+        railRoadCell.setBaseRent(rrBaseRent);
+        railRoadCell.setPrice(rrPrice);
+        return railRoadCell;
     }
 }
